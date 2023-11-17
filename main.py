f3e9f59f2e305e09074e73842fda584891cf3597
ПК <alina.kush2009@gmail.com>
2023-11-17 22:16:26 +0500

    second

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 
 from PyQt5 import uic  # Импортируем uic
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 
 
 class MyWidget(QMainWindow):
@@ -9,6 +9,22 @@ class MyWidget(QMainWindow):
         super().__init__()
         uic.loadUi('main.ui', self)  # Загружаем дизайн
 
+    def select_data(self):
+        # Получим результат запроса,
+        # который ввели в текстовое поле
+        query = '''query'''
+        genre = (self.parameterSelection.currentText(),)
+        res = self.connection.cursor().execute(query, genre).fetchall()
+        # Заполним размеры таблицы
+        self.tableWidget.setColumnCount(7)
+        self.tableWidget.setRowCount(0)
+        # Заполняем таблицу элементами
+        for i, row in enumerate(res):
+            self.tableWidget.setRowCount(
+                self.tableWidget.rowCount() + 1)
+            for j, elem in enumerate(row):
+                self.tableWidget.setItem(
+                    i, j, QTableWidgetItem(str(elem)))
 
 
 if __name__ == '__main__':
